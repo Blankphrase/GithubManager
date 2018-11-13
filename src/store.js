86996da5bad5c98717b2c21d4531e7c6fef4d38b
Blankphrase <ckasera6486@gmail.com>
2018-11-13 09:14:38 +0300

    added vuex state

---
+++ src/store.js
@@ -0,0 +1,101 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(Vuex);
+Vue.use(VueAxios, axios)
+let axiosDefaults = require('axios/lib/defaults');
+axiosDefaults.baseURL = '';
+
+
+export default new Vuex.Store({
+
+    state: {
+        accessToken: '',
+        username: '',
+        profiles: [],
+        repositories: [],
+        commits: [],
+        contents: [],
+        branches: []
+
+    },
+    mutations: {
+
+        UPDATE_ACCESSTOKEN: (state, accessToken) => {
+            state.accessToken = accessToken
+        },
+
+        SET_USERNAME: (state, username) => {
+            state.username = username
+        },
+
+        SET_PROFILES(state, profiles) {
+            state.profiles = profiles
+        },
+        SET_REPOSITORIES(state, repositories) {
+            state.repositories = repositories
+        },
+
+        SET_COMMITS(state, commits) {
+            state.commits = commits
+        },
+        SET_CONTENTS(state, contents) {
+            state.contents = contents
+        },
+        UPDATE_BRANCHES: (state, branches) => {
+            state.branches = branches
+        },
+
+    },
+    actions: {
+
+        loadProfiles({ commit }) {
+            axios
+                .get('https://api.github.com/user?' + localStorage.getItem('accessToken'))
+                .then(r => r.data)
+                .then(profiles => {
+                    commit('SET_PROFILES', profiles)
+                })
+        },
+
+        loadRepositories({ commit }) {
+            let username = this.state.profiles.login;
+            axios
+                .get('https://api.github.com/users/' + localStorage.getItem('userName') + '/repos')
+                .then(r => r.data)
+                .then(repositories => {
+                    commit('SET_REPOSITORIES', repositories)
+                })
+        },
+
+        loadContents({ commit }) {
+            axios
+                .get('https://api.github.com/repos/' + localStorage.getItem('userName') + '/' + localStorage.getItem('repo'))
+                .then(r => r.data)
+                .then(repositories => {
+                    commit('SET_CONTENTS', repositories)
+                })
+        },
+
+        loadCommits({ commit }) {
+            axios
+                .get('https://api.github.com/repos/' + localStorage.getItem('userName') + '/' + localStorage.getItem('repo') + '/' + 'commits')
+                .then(r => r.data)
+                .then(commits => {
+                    commit('SET_COMMITS', commits)
+                })
+        },
+        loadBranches({ commit }) {
+            axios
+                .get('https://api.github.com/repos/' + localStorage.getItem('userName') + '/' + localStorage.getItem('repo') + '/' + 'branches')
+                .then(r => r.data)
+                .then(branches => {
+                    commit('UPDATE_BRANCHES', branches)
+                })
+        },
+
+
+    },
+});
